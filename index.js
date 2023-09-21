0c35c1d6e13e64bed509ca99740fd61193fd2aa1
Guyo <guyo.halake@students.moringaschool.com>
2023-09-21 15:15:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()* 10) + 1;
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4 ;
+let max = Math.max(2,6,10,14,18,20);
 
-// Write your code here
+
